ee224d8b36e69f93e47f6bdb7375d397677e22bb
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-07-05 16:41:52 -0300

    default exports, function exports

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet.js'
+import { Greet } from './components/Greet.js'
 
 function App() {
-  return (
-    <div className="App">
-      <Greet></Greet>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Greet></Greet>
+      </div>
+    );
 }
 
 export default App;
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greet></Greet>
+      </div>
+    );
+  }
+}
+*/
--- front/src/components/Greet.js
+++ front/src/components/Greet.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>hello!!</h1>
-}
+//function Greet() {
+//   return <h1>hello!!</h1>
+//}
 
+export const Greet = () => <h1>hello!! hehe</h1>
+ 
 
-export default Greet;
+//export default Greet;
