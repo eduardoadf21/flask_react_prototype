a135541b21ea959d46f63341c051583e081e073c
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-07-06 20:06:41 -0300

    postUsers prototype

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import {Greet} from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Counter from './components/Counter'
-import Message from './components/Message'
-import Click from './components/Click'
-import ClassCLick from './components/ClassClick'
+import PostUsers from './components/PostUsers';
 
 
 class App extends Component {
@@ -14,8 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Click></Click>
-          <ClassCLick></ClassCLick>
+        <PostUsers></PostUsers>
       </div>
     );
   }
---
+++ front/src/components/PostUsers.js
@@ -0,0 +1,17 @@
+import react, { Component } from 'react'
+import axios from 'axios'
+
+class PostUsers extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>users</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default PostUsers
