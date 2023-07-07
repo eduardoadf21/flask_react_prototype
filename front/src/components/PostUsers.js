12584ac5db9d792dc7cf86639660e718a18a2b41
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-07-07 12:17:26 -0300

    post users

--- front/src/components/PostUsers.js
+++ front/src/components/PostUsers.js
@@ -3,10 +3,35 @@ import axios from 'axios'
 
 class PostUsers extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            users : []
+        }
+    }
+
+    //this method will be executed when the component mounts for the first time, and only once   
+    componentDidMount(){
+        axios.get('http://localhost:5000/users')
+            .then(response => {
+                console.log(response)
+                this.setState({users: response.data})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
+        const { users } = this.state
         return(
             <div>
-                <h1>users</h1>
+                List of users
+                {
+                    users.length ?
+                    users.map(user => <div key={user.username}>{user.fullname}</div>):
+                    null
+                }
             </div>
         )
     }
